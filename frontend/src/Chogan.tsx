85c4c3a6afef24bf5b59cc42f6e962cda6c9f21d
burmanator <theburmanator7@gmail.com>
2021-06-02 14:44:52 -0700

    add to folder components

--- frontend/src/Chogan.tsx
+++ frontend/src/Chogan.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import AuthPage from './pages/AuthPage';
 import './Chogan.css';
-import Home from './pages/home/Home';
+import AddToFolder from './components/AddToFolder';
 
 function Chogan() {
   return (
     <div className="App">
-      {/* <Counter /> */}
-      <Home/>
+      <Counter />
+      {/*this is for debugging*/}
+      <AddToFolder/>
     </div>
   );
 }
---
+++ frontend/src/components/DisplayFolder.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+export default function DisplayFolder(props){
+
+    return (
+        <div>
+            <img src="" alt="folder-symbol"/>
+            <h5>props.folderName</h5>
+        </div>
+    );
+}

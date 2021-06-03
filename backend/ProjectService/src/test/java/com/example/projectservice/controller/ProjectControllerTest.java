f72540bdbabf85f0a16cd6de65e20ec2e927959d
jcowelch <jcowelch@gmail.com>
2021-06-03 16:07:31 -0400

    Added tests for service layer project create (27%)

--- backend/ProjectService/src/test/java/com/example/projectservice/controller/ProjectControllerTest.java
+++ backend/ProjectService/src/test/java/com/example/projectservice/controller/ProjectControllerTest.java
@@ -9,8 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class ProjectControllerTest {
 

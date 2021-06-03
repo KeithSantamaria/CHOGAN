8590cbdcee56e9fa1a3d448fcfad1f6cfef491ee
jcowelch <jcowelch@gmail.com>
2021-06-03 16:29:27 -0400

    Added putmapping for the creation of new projects

--- backend/ProjectService/src/main/java/com/example/projectservice/controller/ProjectController.java
+++ backend/ProjectService/src/main/java/com/example/projectservice/controller/ProjectController.java
@@ -5,10 +5,7 @@ import com.example.projectservice.services.IProjectService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -23,7 +20,7 @@ public class ProjectController {
     * Create
     *
     * */
-    @RequestMapping()
+    @PutMapping("CreateNewProject")
     public ResponseEntity<Project> createNewProject(@RequestBody Project project){
         // two possible solutions, either a successful creation or a failed creation
         try{

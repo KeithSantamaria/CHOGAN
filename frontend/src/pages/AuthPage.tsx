36488e0f0412dbbdc03f19fe5a82cf4ba1106f1e
Keith <keithasantamaria@gmail.com>
2021-06-03 14:22:25 -0500

    Starting dev on AuthPage

---
+++ frontend/src/pages/AuthPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type AuthPageProps = {
+
+};
+
+const AuthPage:React.FC<AuthPageProps> = () => {
+  return (
+    <div>
+      <p>Hi</p>
+    </div>
+  )
+}
+
+export default AuthPage

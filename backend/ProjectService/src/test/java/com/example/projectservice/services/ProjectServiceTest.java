92e0c2103aaf451f09a3fde073a87c0e0114be81
jcowelch <jcowelch@gmail.com>
2021-06-03 16:07:31 -0400

    Added tests for service layer project create (27%)

--- backend/ProjectService/src/test/java/com/example/projectservice/services/ProjectServiceTest.java
+++ backend/ProjectService/src/test/java/com/example/projectservice/services/ProjectServiceTest.java
@@ -1,7 +1,23 @@
 package com.example.projectservice.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.projectservice.models.Project;
+import com.example.projectservice.repository.ProjectRepo;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ProjectServiceTest {
+
+    private ProjectRepo projectRepo = Mockito.mock(ProjectRepo.class);
+
+    private ProjectService projectService = new ProjectService(projectRepo);
+
+    @Test
+    public void insertTest(){
+        Project project = new Project();
+
+        projectService.insert(project);
+    }
 
 }

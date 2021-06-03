8a2c4989f1f36279620130cf31e67b4ddab870b4
jcowelch <jcowelch@gmail.com>
2021-06-03 16:27:38 -0400

    Added model tests (86%)

---
+++ backend/ProjectService/src/test/java/com/example/projectservice/models/UserStoryTest.java
@@ -0,0 +1,31 @@
+package com.example.projectservice.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserStoryTest {
+
+    private UserStory userStory;
+
+    @BeforeEach
+    void setUp() {
+        userStory = new UserStory();
+    }
+
+    @Test
+    void getUserStoryId() {
+        userStory.setUserStoryId("Id");
+        String id = userStory.getUserStoryId();
+        Assertions.assertEquals(id,"Id");
+    }
+
+    @Test
+    void getUserStoryDescription() {
+        userStory.setUserStoryDescription("Desc");
+        String desc = userStory.getUserStoryDescription();
+        Assertions.assertEquals(desc,"Desc");
+    }
+}

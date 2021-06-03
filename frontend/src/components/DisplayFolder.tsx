e2c8473f7a30f2942ef00207727f091596c35aa0
Hunter Buxton <hunter.buxton@protonmail.com>
2021-06-02 18:54:48 -0600

    fixed type error from DisplayFolder props

--- frontend/src/components/DisplayFolder.tsx
+++ frontend/src/components/DisplayFolder.tsx
@@ -1,11 +1,15 @@
 import React, {useState} from 'react';
 
-export default function DisplayFolder(props){
+type DisplayFolderProps = {
+    folderName: string
+}
+
+export default function DisplayFolder({ folderName }: DisplayFolderProps) {
 
     return (
         <div>
             <img src="" alt="folder-symbol"/>
-            <h5>props.folderName</h5>
+            <h5>{folderName}</h5>
         </div>
     );
 }

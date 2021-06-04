6650dfea4393ef9413b03f8a77114738f1afb596
Hunter Buxton <hunter.buxton@protonmail.com>
2021-06-04 06:51:09 -0600

    added close button to project actions modal and project folders modal

--- frontend/src/components/ProjectActionsModal.tsx
+++ frontend/src/components/ProjectActionsModal.tsx
@@ -17,6 +17,10 @@ export default function ProjectActionsModal() {
 
     return (
         <div>
+            {/* <div className='exit-button'><Button className='exit-button' onClick={handleClose}>X</Button></div> */}
+            <Modal.Header closeButton>
+                <Modal.Title>Actions</Modal.Title>
+            </Modal.Header>
             <Form>
                 <Button className="project-actions-modal-buttons" onClick={addToFolderAction} >Add To Folder</Button>
                 <Button className="project-actions-modal-buttons" onClick={cloneAction}>Clone</Button>
@@ -26,9 +30,10 @@ export default function ProjectActionsModal() {
             </Form>
 
             <Modal show={show} onHide={handleClose}>
-                <Modal show={show} onHide={handleClose}>
-                    <AddToFolder/>
-                </Modal>
+                <Modal.Header closeButton>
+                    <Modal.Title>Folders</Modal.Title>
+                </Modal.Header>
+                <AddToFolder/>
             </Modal>
         </div>
     );

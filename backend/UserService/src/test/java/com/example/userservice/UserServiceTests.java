a49739108fe8370bb57c2b0dfcf2539f24306e4b
Michael <mfong5@illinois.edu>
2021-06-05 02:49:31 -0500

    Issue 27 - Wrote out methods for the service and added a couple for the repo, test skeletons

---
+++ backend/UserService/src/test/java/com/example/userservice/UserServiceTests.java
@@ -0,0 +1,49 @@
+package com.example.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTests {
+
+    @Test
+    void testNewUser() {
+
+    }
+
+    @Test
+    void updateUser() {
+
+    }
+
+    @Test
+    void testSetFullName() {
+
+    }
+
+    @Test
+    void testGetUserByUsername() {
+
+    }
+
+    @Test
+    void testGetUserById() {
+
+    }
+
+    @Test
+    void testUserLogIn() {
+
+    }
+
+    @Test
+    void testChangePassword() {
+
+    }
+
+    @Test
+    void testCheckSecurityQuestion() {
+
+    }
+    
+}

40c92185e6fa85ab2bfba3570a7f24472f3df291
manny <33538272+Basic-Sombrero@users.noreply.github.com>
2021-06-05 18:31:22 -0700

    Update WebSecurityConfig.java

--- backend/UserService/src/main/java/com/example/userservice/configuration/WebSecurityConfig.java
+++ backend/UserService/src/main/java/com/example/userservice/configuration/WebSecurityConfig.java
@@ -31,6 +31,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                     .and()
                 .logout()
+                    .logoutUrl("/logout")
+                    .deleteCookies()
                     .permitAll();
     }
 

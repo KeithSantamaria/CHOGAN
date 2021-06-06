ef144ed54bd68555c9412d50708aff5d7509ba89
sam laitha <samlaitha@sams-MacBook-Pro.local>
2021-06-06 02:01:03 -0700

    adding sorting functionality

--- frontend/src/components/home/GridView.component.tsx
+++ frontend/src/components/home/GridView.component.tsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import FolderCard from '../../components/home/FolderCard.component';
 import ProjectCard from '../../components/home/ProjectCard.component';
 
 import {Col, Row} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faSortDown} from "@fortawesome/free-solid-svg-icons";
+import {faSortDown, faSortUp} from "@fortawesome/free-solid-svg-icons";
 
 
 
 export default function GridView(props: any) {
+    const [active, setActive] = useState(false);
+    const [sortedArray, setSortedArray] = useState([]);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        if(active) {
+            props.projects.sort((a:any, b:any) => a.name > b.name ? 1 : -1)
+            setSortedArray(props.projects);
+        } else {
+            props.projects.sort((a:any, b:any) => a.name > b.name ? -1 : 1)
+            setSortedArray(props.projects);
+        }
+
+    },[active]);
+
+    const toggle = () => { 
+        if(active === false) {
+            setActive(true);
+            console.log("Sort" + JSON.stringify(sortedArray));
+        }
+        if(active === true) {
+            setActive(false);
+            console.log("Not" + JSON.stringify(sortedArray));
+        }
+        return null;
+    }
+
+    let className='sort';
+    if(active) {
+        className += ' sort-active';
+    } else {
+        className='sort';
+    }
+
   return (
     <>
         <Row className="folder-container-wrapper">
@@ -17,9 +51,9 @@ export default function GridView(props: any) {
             </Col>
 
             <Col className="row-2-col-2" >
-                <div style={{float: 'right'}}>
+                <div style={{float: 'right'}} className={className + " grid-sort-down"} onClick={toggle}>
                     <span style={{paddingRight: '8px'}}>Name</span>
-                    <FontAwesomeIcon className="fa-icon" icon={faSortDown}/>
+                    {active ? <FontAwesomeIcon className="fa-icon fa-1x" icon={faSortDown}/> : <FontAwesomeIcon className="fa-icon fa-1x" icon={faSortUp}/>}
                 </div>
                 
             </Col>
@@ -33,7 +67,7 @@ export default function GridView(props: any) {
             </Col>
         </Row>
 
-        <ProjectCard projects={props.projects}/>
+        <ProjectCard projects= {sortedArray}/>
 
     </>
   );

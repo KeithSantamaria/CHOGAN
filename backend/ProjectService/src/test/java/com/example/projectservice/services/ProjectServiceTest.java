30d55faa78db95a92a5500dfe50b7119465f4e44
jcowelch <jcowelch@gmail.com>
2021-06-07 14:10:10 -0400

    Added test for service for findbyprojectId, (89%)

--- backend/ProjectService/src/test/java/com/example/projectservice/services/ProjectServiceTest.java
+++ backend/ProjectService/src/test/java/com/example/projectservice/services/ProjectServiceTest.java
@@ -3,6 +3,7 @@ package com.example.projectservice.services;
 import com.projectservice.models.Project;
 import com.projectservice.repository.ProjectRepo;
 import com.projectservice.services.ProjectService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.SpringBootConfiguration;
@@ -32,6 +33,17 @@ class ProjectServiceTest {
     * Read
     *
     * */
+    @Test
+    public void findByProjectId(){
+        Project project = new Project();
+        String projectId = "id";
+
+        Mockito.when(projectRepo.findByProjectId(projectId)).thenReturn(project);
+
+        Project foundProject = projectService.findByProjectId(projectId);
+
+        Assertions.assertEquals(foundProject,project);
+    }
 
     /*
     *

b046bb93c000e676288eb513e3194a986790941b
Hunter Buxton <hunter.buxton@protonmail.com>
2021-06-07 01:07:49 -0600

    finished project modals css

--- frontend/src/components/home/modal/AddToFolder.tsx
+++ frontend/src/components/home/modal/AddToFolder.tsx
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
 import DisplayFolder from "./DisplayFolder";
 import { Modal } from 'react-bootstrap';
+import "../../../css/home/PopUpModal/popUpModal.css"
+
 export default function AddToFolder(){
     const [data, setData] = useState(["school","work"]);
     //
 
     return (
         <>
-            <Modal.Header className="sub-modal" closeButton>
+            <Modal.Header className="sub-modal action-header" closeButton>
                 <Modal.Title>Folders</Modal.Title>
             </Modal.Header>
             <Modal.Body className="sub-modal-body">

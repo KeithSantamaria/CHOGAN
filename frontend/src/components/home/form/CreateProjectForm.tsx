ef099880b1b0b815d836f6a32edfea0cd18271de
sam laitha <samlaitha@sams-MacBook-Pro.local>
2021-06-08 14:50:58 -0700

    adding styles to modal and create button

--- frontend/src/components/home/form/CreateProjectForm.tsx
+++ frontend/src/components/home/form/CreateProjectForm.tsx
@@ -1,38 +1,47 @@
 import {Button, Form} from "react-bootstrap";
 import Container from "react-bootstrap/Container";
-import React, {FormEvent, useState} from "react";
-import Modal from "react-bootstrap/Modal";
+import {FormEvent, useState} from "react";
 
 export default function CreateProjectForm(props:any){
     const [projectName, setProjectName] = useState("");
     const [description, setDescription]= useState("");
 
-    // React.useEffect(()=>{
-    //     alert("")
-    // },[projectName]);
-
     const submit =(e:FormEvent)=> {
         e.preventDefault();
-        // return alert(projectName+"   "+ description);
         alert("qye pasosoo" + projectName+description);
     }
 
     return(
-    <Container>
+    <Container className="create-proj-form-container">
         <Form >
-            <Form.Group >
-                <Form.Label>ProjectTitle</Form.Label>
-                <Form.Control placeholder="ProjectTitle"
-                              onChange={(e)=>setProjectName(e.target.value)}/>
+            <h4>New Project</h4>
+            <div>Create new project name and Description</div>
+            <div>Manage your project</div>
+
+            <Form.Group className="project-name-wrapper">
+                <Form.Label>Project Name</Form.Label>
+                <Form.Control
+                    placeholder="Enter project name"
+                    onChange={(e)=>setProjectName(e.target.value)}/>
             </Form.Group>
             <Form.Group>
                 <Form.Label>Description</Form.Label>
-                <Form.Control as="textarea" rows={3}
-                              onChange={(e)=>setDescription(e.target.value)}/>
+                <Form.Control
+                    as="textarea"
+                    className="modal-create-form-textarea"
+                    rows={5}
+                    onChange={(e)=>setDescription(e.target.value)}
+                />
             </Form.Group>
 
-            <Button variant="primary" onClick={submit}>Create</Button>
-            <Button variant="secondary" onClick={props.handleClose}>Close</Button>
+            <span style={{float: 'right'}} className="btn-group-wrapper">
+                <span>
+                    <Button variant="outline" onClick={props.handleClose}>Close</Button>
+                </span>
+               
+                <Button variant="primary" onClick={submit}>Create</Button>
+            </span>
+           
         </Form>
     </Container>
 );

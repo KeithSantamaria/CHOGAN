92420eabec4d9716678edd73d98c3300b5c51973
scbarnard01 <39101603+scbarnard@users.noreply.github.com>
2021-06-08 16:16:19 -0400

    v2.0

--- frontend/src/components/ProjectGeneralInfo.tsx
+++ frontend/src/components/ProjectGeneralInfo.tsx
@@ -1,29 +1,20 @@
 import React, { useMemo } from 'react'
 import axios from 'axios';
 import ProjectSideNav from './ProjectSideNav'
-import { useAppDispatch } from "../redux/hooks"
+import WidgetForm from './WidgetForm';
+import { useAppDispatch, useAppSelector } from "../redux/hooks"
 import { Jumbotron, Card, Button, Modal } from 'react-bootstrap';
+import { selectProjectApp, setProject } from '../redux/projectAppSlice';
 
 // For production, project will be passed as a prop from Project Card (built by Home group)
-
 // const ProjectGeneralInfo = ({project} : any ) => {
 const ProjectGeneralInfo = () => {
 
-    const [modalShow, setModalShow] = React.useState(false);
+  const dispatch = useAppDispatch();
+  const projectAppState= useAppSelector(selectProjectApp);
+  const [modalShow, setModalShow] = React.useState(false);
     
-    const [project, setProject] = React.useState("");
-    const [projectId, setProjectId] = React.useState("");
-    const [projectName, setProjectName] = React.useState("");
-    const [projectDescription, setProjectDescription] = React.useState("");
-    const [projectStatus, setProjectStatus] = React.useState("");
 
-    const [endpoints, setEndpoints] = React.useState("");
-    const [models, setModels] = React.useState("");
-    const [tags, setTags] = React.useState("");
-    const [userStories, setUserStories] = React.useState("");
-
-    // const dispatch = useAppDispatch();
-  
     // Code for grabbing user from User Slice
 
     const handleOpen = () => setModalShow(true);
@@ -35,48 +26,17 @@ const ProjectGeneralInfo = () => {
 
         // Production query string; uncomment when ready to test prod
         // const queryString = `http://localhost:42069/api/read/project?projectId=${projectId}`; 
-
         axios
           .get(queryString)
           .then((response) => {
             console.log("response", response);
-            setProject(response.data);
-            setProjectId(response.data.projectId);
-            setProjectName(response.data.projectName);
-            setProjectDescription(response.data.projectDescription);
+            const projectData = response.data;
+            dispatch(setProject(projectData));
           })
           .catch((error) => {
             console.log("There was an error: ", error);
           });
       };
-
-
-
-      // const updateProjectStatus = (event: any, project: any) => {
-      //   const queryString = `http://localhost:42069/api/update/project/status`;
-      //   const body = {
-      //     params: {
-      //       projectId: project.projectId,
-      //       projectStatus: event.target.value,
-      //     },
-      //   };
-      //   console.log(body);
-      //   axios
-      //     .put(queryString, body)
-      //     .then((response) => {
-      //       console.log("response", response);
-      //       if(response.status === 200){
-      //         console.log(response.data);
-      //         setProject(response.data);
-      //         setProjectId(response.data.projectId);
-      //         setProjectName(response.data.projectName);
-      //         setProjectDescription(response.data.projectDescription);
-      //       }
-      //     })
-      //     .catch((error) => {
-      //       console.log("There was an error: ", error);
-      //     })
-      // };
 
       useMemo(() => {
         getProject();
@@ -97,27 +57,36 @@ const ProjectGeneralInfo = () => {
                 </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-
+                  <WidgetForm project={projectAppState.project}/>
                 </Modal.Body>
-                <Modal.Footer>
-                 <Button onClick={ handleClose }>Close</Button>
-                </Modal.Footer>
           </Modal>
         )
     }
     return (
         <div>
-          {/* <ProjectSideNav /> */}
+          <ProjectSideNav />
           <Card>
             <Card.Body>
               {/* <Card.Title>{userAppState.user.userName} - {projectName}</Card.Title> */}
-              <Card.Title>{projectName}</Card.Title>
+              <Card.Title>{projectAppState.project.projectName}</Card.Title>
 
               <Card.Text>
-                {projectDescription}
+              {projectAppState.project.projectDescription}
               </Card.Text>
             </Card.Body>
           </Card>
+          {projectAppState.project.widgets.map( (widget: any) => {
+            return(
+              <Card>
+                <Card.Body>
+                  <Card.Title>{widget.widgetName}</Card.Title>
+                  <Card.Text>
+                    {widget.widgetDescription}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            );
+          })}
           <Button variant="primary" onClick={handleOpen}>
             New Widget
           </Button>
--- frontend/src/components/WidgetForm.tsx
+++ frontend/src/components/WidgetForm.tsx
@@ -1,9 +1,57 @@
+import axios from 'axios';
 import React from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { useAppDispatch, useAppSelector } from "../redux/hooks"
+import { selectProjectApp, setCreateNewWidgetForm, setProject } from '../redux/projectAppSlice';
 
-const WidgetForm = ()=> {
+const WidgetForm = (project:any)=> {
+    
+    const projectAppState = useAppSelector(selectProjectApp);
+    const dispatch = useAppDispatch();
+
+    const formChangeHandler = (event: any) => {
+        const fieldName = event.target.name;
+        const value = event.target.value;
+        dispatch(setCreateNewWidgetForm({ fieldName, value }));
+      };
+
+    const updateProject = () => {
+        const queryString = `http://localhost:42069/api/update/project`;
+        
+        const body = {
+            params: {
+              project: projectAppState.project,
+            },
+          };
+        // Test query string works; comment when ready to test prod
+        // const queryString = `http://localhost:42069/api/read/project?projectId=60bc36b65d2b0da1deb9ada2`;
+        // Production query string; uncomment when ready to test prod
+        // const queryString = `http://localhost:42069/api/read/project?projectId=${projectId}`; 
+        axios
+          .put(queryString, body)
+          .then((response) => {
+            console.log("response", response);
+            const projectData = response.data;
+            dispatch(setProject(projectData));
+          })
+          .catch((error) => {
+            console.log("There was an error: ", error);
+          });
+      };
+
     return (
         <div>
-            
+            <Form>            
+            <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Widget Name</Form.Label>
+                <Form.Control name="widgetName" type="text" placeholder="Documentation" onChange={formChangeHandler} />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control name="widgetDescription" as="textarea" rows={3} onChange={formChangeHandler} />
+            </Form.Group>
+            <Button onClick={updateProject}>Save</Button>
+            </Form>
         </div>
     )
 }

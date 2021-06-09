6d07de5834c1936a3c40d28d2c2f8cb4ebdfc902
TJ <tom.j.martinez91@gmail.com>
2021-06-08 20:16:59 -0700

    homelistview add fontawesome import

--- frontend/src/Chogan.tsx
+++ frontend/src/Chogan.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-
-import AuthPage from './pages/AuthPage';
 import './Chogan.css';
 import AddToFolder from './components/AddToFolder';
+
 
 function Chogan() {
   return (
     <div className="App">
-      <Counter />
-      {/*this is for debugging*/}
-      <AddToFolder/>
+      <Home />
     </div>
   );
 }

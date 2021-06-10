c32ec0dba69e78709d57588dc7471bd8da57236e
TJ <23747199+tomjmartinez@users.noreply.github.com>
2021-06-10 09:59:59 -0700

    merge conflicts resolved

--- frontend/src/components/ProjectERDs.tsx
+++ frontend/src/components/ProjectERDs.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
-<<<<<<< Updated upstream
 import ProjectSideNav from './ProjectSideNav'
-=======
->>>>>>> Stashed changes
 
 function ProjectERDs() {
     return (
         <div>
-<<<<<<< Updated upstream
             <ProjectSideNav />
             <p>ERDs</p>
-=======
-            
->>>>>>> Stashed changes
         </div>
     )
 }

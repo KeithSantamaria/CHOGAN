a8fb7c55b96b8570068813dbe0474bb975cbe1a5
jcowelch <jcowelch@gmail.com>
2021-06-10 11:29:27 -0400

    Tests for wireframes and erds (56%)

--- backend/ProjectService/src/main/java/com/projectservice/models/Wireframe.java
+++ backend/ProjectService/src/main/java/com/projectservice/models/Wireframe.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+/**
+ * Wireframe pojo for handling wireframes and images
+ */
 @Data
 @Document(collection="wireframes")
 public class Wireframe {
--- backend/ProjectService/src/test/java/com/projectservice/models/ERDTest.java
+++ backend/ProjectService/src/test/java/com/projectservice/models/ERDTest.java
@@ -1,4 +1,50 @@
 package com.projectservice.models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class ERDTest {
+
+    private ERD erd;
+
+    @BeforeEach
+    void setUp(){
+        erd = new ERD();
+    }
+
+    @Test
+    void erdIDTest(){
+        erd.setErdID("Id");
+        String id = erd.getErdID();
+        Assertions.assertEquals(id,"Id");
+    }
+
+    @Test
+    void projectIdTest(){
+        erd.setProjectId("Id");
+        String id = erd.getProjectId();
+        Assertions.assertEquals(id,"Id");
+    }
+
+    @Test
+    void erdNameTest(){
+        erd.setErdName("Name");
+        String name = erd.getErdName();
+        Assertions.assertEquals(name,"Name");
+    }
+
+    @Test
+    void erdDescriptionTest(){
+        erd.setErdDescription("Desc");
+        String desc = erd.getErdDescription();
+        Assertions.assertEquals(desc,"Desc");
+    }
+
+    @Test
+    void erdImageUrlTest(){
+        erd.setErdImageUrl("URL");
+        String url = erd.getErdImageUrl();
+        Assertions.assertEquals(url,"URL");
+    }
 }

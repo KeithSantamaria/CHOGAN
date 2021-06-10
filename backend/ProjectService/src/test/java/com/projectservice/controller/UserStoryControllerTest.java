918231ac9747e7c36ee52927ba201f32665402f9
jcowelch <jcowelch@gmail.com>
2021-06-10 16:59:06 -0400

    Initial setup for user story (93%)

--- backend/ProjectService/src/test/java/com/projectservice/controller/UserStoryControllerTest.java
+++ backend/ProjectService/src/test/java/com/projectservice/controller/UserStoryControllerTest.java
@@ -1,4 +1,36 @@
 package com.projectservice.controller;
 
+import com.projectservice.services.IUserStoryService;
+import com.projectservice.services.UserStoryService;
+import org.mockito.Mockito;
+
 public class UserStoryControllerTest {
+
+    private final IUserStoryService userStoryService = Mockito.mock(UserStoryService.class);
+
+    private final UserStoryController userStoryController = new UserStoryController(userStoryService);
+
+    /*
+    *
+    * Create
+    *
+    * */
+
+    /*
+    *
+    * Read
+    *
+    * */
+
+    /*
+    *
+    * Update
+    *
+    * */
+
+    /*
+    *
+    * Delete
+    *
+    * */
 }
--- backend/ProjectService/src/test/java/com/projectservice/services/UserStoryServiceTest.java
+++ backend/ProjectService/src/test/java/com/projectservice/services/UserStoryServiceTest.java
@@ -1,4 +1,35 @@
 package com.projectservice.services;
 
+import com.projectservice.repository.UserStoryRepo;
+import org.mockito.Mockito;
+
 public class UserStoryServiceTest {
+
+    private final UserStoryRepo userStoryRepo = Mockito.mock(UserStoryRepo.class);
+
+    private final UserStoryService userStoryService = new UserStoryService(userStoryRepo);
+
+    /*
+    *
+    * Create
+    *
+    * */
+
+    /*
+    *
+    * Read
+    *
+    * */
+
+    /*
+    *
+    * Update
+    *
+    * */
+
+    /*
+    *
+    * Delete
+    *
+    * */
 }

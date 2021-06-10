a04c33afa2e5a96633f317be8517446992925d86
KeithSantamaria <36080275+KeithSantamaria@users.noreply.github.com>
2021-06-10 12:18:59 -0500

    Add a sec config that doesnt kill my sanity

--- backend/UserService/src/main/java/com/example/userservice/configuration/WebSecurityConfig.java
+++ backend/UserService/src/main/java/com/example/userservice/configuration/WebSecurityConfig.java
@@ -23,9 +23,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/user").permitAll()
-                .anyRequest().authenticated();
+          .antMatchers("/user").permitAll()
+          .anyRequest().authenticated()
+          .and()
+          .csrf().disable();
     }
 
 
 }
+
+
+//        http.authorizeRequests()
+//          .antMatchers("/user").permitAll()
+//          .anyRequest().authenticated();

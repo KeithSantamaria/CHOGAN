7fab3a44c9d0275dece8f3b442069c92bab36c69
jcowelch <jcowelch@gmail.com>
2021-06-10 14:14:09 -0400

    Initial test files for models

--- backend/ProjectService/src/test/java/com/projectservice/controller/ModelControllerTest.java
+++ backend/ProjectService/src/test/java/com/projectservice/controller/ModelControllerTest.java
@@ -1,4 +1,37 @@
 package com.projectservice.controller;
 
+import com.projectservice.services.IModelService;
+import com.projectservice.services.ModelService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class ModelControllerTest {
+
+    private final IModelService modelService = Mockito.mock(ModelService.class);
+
+    private final ModelController modelController = new ModelController(modelService);
+
+    /*
+    *
+    * Create
+    *
+    * */
+
+    /*
+    *
+    * Read
+    *
+    * */
+
+    /*
+    *
+    * Update
+    *
+    * */
+
+    /*
+    *
+    * Delete
+    *
+    * */
 }
--- backend/ProjectService/src/test/java/com/projectservice/services/ModelServiceTest.java
+++ backend/ProjectService/src/test/java/com/projectservice/services/ModelServiceTest.java
@@ -1,4 +1,35 @@
 package com.projectservice.services;
 
+import com.projectservice.repository.ModelRepo;
+import org.mockito.Mockito;
+
 public class ModelServiceTest {
+
+    private final ModelRepo modelRepo = Mockito.mock(ModelRepo.class);
+
+    private final ModelService modelService = new ModelService(modelRepo);
+
+    /*
+    *
+    * Create
+    *
+    * */
+
+    /*
+    *
+    * Read
+    *
+    * */
+
+    /*
+    *
+    * Update
+    * 
+    * */
+
+    /*
+    *
+    * Delete
+    *
+    * */
 }

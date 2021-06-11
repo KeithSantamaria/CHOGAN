3ee7572c3c75fc73fd378de26b4afaa041201135
jcowelch <jcowelch@gmail.com>
2021-06-11 13:05:07 -0400

    Added tests for wireframe service (98%)

--- backend/ProjectService/src/test/java/com/projectservice/services/WireframeServiceTest.java
+++ backend/ProjectService/src/test/java/com/projectservice/services/WireframeServiceTest.java
@@ -1,8 +1,13 @@
 package com.projectservice.services;
 
+import com.projectservice.models.Wireframe;
 import com.projectservice.repository.WireframeRepo;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WireframeServiceTest {
 
@@ -18,7 +23,8 @@ public class WireframeServiceTest {
 
     @Test
     void insertTest(){
-
+        Wireframe wireframe = new Wireframe();
+        wireframeService.insert(wireframe);
     }
 
     /*
@@ -29,12 +35,26 @@ public class WireframeServiceTest {
 
     @Test
     void findByWireframeIdTest(){
+        String wireframeId = "Id";
+        Wireframe wireframe = new Wireframe();
 
+        Mockito.when(wireframeRepo.findByWireframeId(wireframeId)).thenReturn(wireframe);
+
+        Wireframe foundWireframe = wireframeService.findByWireframeId(wireframeId);
+
+        Assertions.assertEquals(foundWireframe,wireframe);
     }
 
     @Test
     void findByProjectIdTest(){
+        String projectId = "Id";
+        List<Wireframe> list = new ArrayList<>();
 
+        Mockito.when(wireframeRepo.findByProjectId(projectId)).thenReturn(list);
+
+        List<Wireframe> foundList = wireframeService.findByProjectId(projectId);
+
+        Assertions.assertEquals(foundList,list);
     }
 
     /*
@@ -45,7 +65,13 @@ public class WireframeServiceTest {
 
     @Test
     void updateWireframeTest(){
+        Wireframe wireframe = new Wireframe();
 
+        Mockito.when(wireframeRepo.save(wireframe)).thenReturn(wireframe);
+
+        Wireframe foundWireframe = wireframeService.updateWireframe(wireframe);
+
+        Assertions.assertEquals(foundWireframe,wireframe);
     }
 
     /*
@@ -56,11 +82,27 @@ public class WireframeServiceTest {
 
     @Test
     void deleteWireframeSuccessTest(){
+        String wireframeId = "Id";
+        Wireframe wireframe = new Wireframe();
+        wireframe.setProjectId("pId");
+        List<Wireframe> list = new ArrayList<>();
 
+        Mockito.when(wireframeRepo.findByWireframeId(wireframeId)).thenReturn(wireframe);
+        Mockito.when(wireframeRepo.findByProjectId("pId")).thenReturn(list);
+
+        List<Wireframe> foundList = wireframeService.deleteWireframe(wireframeId);
+
+        Assertions.assertEquals(foundList,list);
     }
 
     @Test
     void deleteWireframeFailureTest(){
+        String wireframeId = "Id";
 
+        Mockito.when(wireframeRepo.findByWireframeId(wireframeId)).thenReturn(null);
+
+        List<Wireframe> foundList = wireframeService.deleteWireframe(wireframeId);
+
+        Assertions.assertNull(foundList);
     }
 }

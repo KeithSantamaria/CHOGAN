9bab4cff299a772a9f044bb9d7e639e1180b2aae
jcowelch <jcowelch@gmail.com>
2021-06-11 09:52:02 -0400

    Added initial support for testing widgets

--- backend/ProjectService/src/test/java/com/projectservice/controller/WidgetControllerTest.java
+++ backend/ProjectService/src/test/java/com/projectservice/controller/WidgetControllerTest.java
@@ -1,4 +1,36 @@
 package com.projectservice.controller;
 
+import com.projectservice.services.IWidgetService;
+import com.projectservice.services.WidgetService;
+import org.mockito.Mockito;
+
 public class WidgetControllerTest {
+
+    private final IWidgetService widgetService = Mockito.mock(WidgetService.class);
+
+    private final WidgetController widgetController = new WidgetController(widgetService);
+
+    /*
+    *
+    * Create
+    *
+    * */
+
+    /*
+    *
+    * Read
+    *
+    * */
+
+    /*
+    *
+    * Update
+    *
+    * */
+
+    /*
+    *
+    * Delete
+    *
+    * */
 }
--- backend/ProjectService/src/test/java/com/projectservice/services/WidgetServiceTest.java
+++ backend/ProjectService/src/test/java/com/projectservice/services/WidgetServiceTest.java
@@ -1,4 +1,35 @@
 package com.projectservice.services;
 
+import com.projectservice.repository.WidgetRepo;
+import org.mockito.Mockito;
+
 public class WidgetServiceTest {
+
+    private final WidgetRepo widgetRepo = Mockito.mock(WidgetRepo.class);
+
+    private final WidgetService widgetService = new WidgetService(widgetRepo);
+
+    /*
+    *
+    * Create
+    *
+    * */
+
+    /*
+    *
+    * Read
+    *
+    * */
+
+    /*
+    *
+    * Update
+    *
+    * */
+
+    /*
+    *
+    * Delete
+    *
+    * */
 }

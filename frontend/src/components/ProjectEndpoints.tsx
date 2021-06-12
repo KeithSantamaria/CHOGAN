bbd6e5b176ccb158d17e556ebce4796857148a00
scbarnard01 <39101603+scbarnard@users.noreply.github.com>
2021-06-11 20:19:08 -0400

    working EndpointCard component

--- frontend/src/components/ProjectEndpoints.tsx
+++ frontend/src/components/ProjectEndpoints.tsx
@@ -2,9 +2,10 @@ import axios from "axios";
 import React, { useMemo } from "react";
 import { Button, Card, Modal } from "react-bootstrap";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import { selectProjectApp, setProject, setWidgets } from "../redux/projectAppSlice";
+import { selectProjectApp, setProject, setEndpoints } from "../redux/projectAppSlice";
 import ProjectSideNav from "./ProjectSideNav";
-import WidgetForm from "./WidgetForm";
+import EndpointForm from "./EndpointForm";
+import EndpointCard from "./EndpointCard";
 
 function ProjectEndpoints() {
   const projectAppState = useAppSelector(selectProjectApp);
@@ -13,18 +14,22 @@ function ProjectEndpoints() {
   const handleClose = () => setModalShow(false);
   const dispatch = useAppDispatch();
 
-  const getProject = () => {
+  const getEndpoints= () => {
     // Test query string works; comment when ready to test prod
     const queryString = `http://localhost:42069/api/read/project/endpoints`;
-
+    const body = {
+      params: {
+        projectId: "60bc36b65d2b0da1deb9ada2"
+      }
+    }
     // Production query string; uncomment when ready to test prod
     // const queryString = `http://localhost:42069/api/read/project?projectId=${projectId}`;
     axios
-      .get(queryString)
+      .get(queryString, body)
       .then((response) => {
         console.log("response", response);
-        const projectData = response.data;
-        dispatch(setProject(projectData));
+        const endpointData = response.data;
+        dispatch(setEndpoints(endpointData));
       })
       .catch((error) => {
         console.log("There was an error: ", error);
@@ -32,7 +37,7 @@ function ProjectEndpoints() {
   };
 
   useMemo(() => {
-    getProject();
+    getEndpoints();
   }, []);
 
   return (
@@ -41,18 +46,13 @@ function ProjectEndpoints() {
 
       {projectAppState.endpoints.map((endpoint: any) => {
         return (
-          <Card>
-            <Card.Body>
-              <Card.Title>{endpoint.endpointName}</Card.Title>
-              <Card.Text>{endpoint.endpointDescription}</Card.Text>
-              <Card.Text>{endpoint.endpointUrlPattern}</Card.Text>
-            </Card.Body>
-          </Card>
+          <EndpointCard endpoint={endpoint} />
         );
       })}
       <Button variant="primary" onClick={handleOpen}>
         New Endpoint
       </Button>
+      
     </div>
   );
 }

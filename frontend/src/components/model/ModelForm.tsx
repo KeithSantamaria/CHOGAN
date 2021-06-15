df3e165232c110aa5ced9ebf7e036f54bb250f4d
scbarnard01 <39101603+scbarnard@users.noreply.github.com>
2021-06-15 09:35:03 -0400

    v8; added Ray's model method

--- frontend/src/components/model/ModelForm.tsx
+++ frontend/src/components/model/ModelForm.tsx
@@ -1,5 +1,8 @@
 import axios from "axios";
-import { Button, Form, Table, Container } from "react-bootstrap";
+import React from "react";
+//added
+import {useState} from "react";
+import { Button, Form, Table } from "react-bootstrap";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import {
   resetCreateNewModelForm,
@@ -8,12 +11,33 @@ import {
   setModels,
 } from "../../redux/projectAppSlice";
 
-import '../../css/project-service/model-form.css';
+//added
+var tempArrayAll=new Array();
+var tempData={} as any;
+//added
+function useForceUpdate(){
+    const [value, setValue] = useState(0); // integer state
+    return () => setValue(value => value + 1); // update the state to force render
+}
 
 const ModelForm = () => {
   const projectAppState = useAppSelector(selectProjectApp);
   const dispatch = useAppDispatch();
+  const fieldRef= React.createRef() as any;
+  const typeRef= React.createRef() as any;
 
+  //added
+  //1. change input boxes to empty on on addRow() DONE
+  //2. send tempData JSON to modelData through dispatch
+  //3. add overflow DONE AUTOMACTICALLY
+  //4. changed the invocation to formChangeHandler to be call once on submission.
+  //5. check for duplicate name submissions. DONE
+  const forceUpdate = useForceUpdate();
+  var tempArray=new Array();
+
+
+
+  //added
   const formChangeHandler = (event: any) => {
     const fieldName = event.target.name;
     const value = event.target.value;
@@ -27,20 +51,20 @@ const ModelForm = () => {
     const queryString = `http://localhost:42069/api/create/project/model`;
     if (
       projectAppState.createNewModelForm.modelName === "" ||
-      projectAppState.createNewModelForm.modelMetadata === []
+      tempData === {}
     ) {
-      alert("There is nothing to add");
+      alert("There is nothing to add.");
     } else {
       const model = {
         modelName: projectAppState.createNewModelForm.modelName,
-        modelMetadata: projectAppState.createNewModelForm.modelMetadata,
+        modelMetadata: tempData,
         //production
         //projectId: projectAppState.project.projectId,
 
         //test
         projectId: projectId,
       };
-
+      console.log(model);
       axios
         .post(queryString, model)
         .then((response) => {
@@ -56,23 +80,56 @@ const ModelForm = () => {
   };
 
   const addRow = () => {
+    if(tempArray.length<2){
+      alert("You must fill out all fields before submitting.");
+      tempArray=[];
+      fieldRef.current.value="";
+      typeRef.current.value="";
+      return;
+    }
 
+    else if(tempArray[0] in tempData){
+      alert("Field name already exists. All names must be unique.");
+      tempArray=[];
+      fieldRef.current.value="";
+      typeRef.current.value="";
+      return;
+    }
+
+    tempArrayAll.push(tempArray);
+    tempData[tempArray[0]]=tempArray[1];
+    for(var key in tempData){ /*alert("key: "+key+" , value: "+tempData[key]);*/}
+    forceUpdate();
+    tempArray=[];
+    fieldRef.current.value="";
+    typeRef.current.value="";
   }
 
+  var generateAddedRows = () => {
+    var rows=tempArrayAll.map((i) => <tr><th style={{color: "lightslategray"}}>{i[0]}</th> <th style={{color: "lightslategray"}}>{i[1]}</th></tr>);
+    return rows
+  }
+
+  var addField=(event: any)=>{tempArray[0]=event.target.value;}
+
+  var addType=(event: any)=>{tempArray[1]=event.target.value;}
+
   return (
-    <Container className="create-proj-form-container">
+    <div>
       <Form>
-        <Form.Group className="project-name-wrapper" controlId="exampleForm.ControlInput1">
+        <Form.Group controlId="exampleForm.ControlInput1">
           <Form.Label>Model Name</Form.Label>
           <Form.Control
             name="modelName"
             type="text"
-            // placeholder="Employees"
             onChange={formChangeHandler}
           />
         </Form.Group>
         <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Table id="metadataTable" size="sm">
+          <Table id="metadataTable" striped bordered hover size="sm">
+
+            {generateAddedRows()}
+
             <tr>
               <th>Field</th>
               <th>Type</th>
@@ -80,27 +137,29 @@ const ModelForm = () => {
             <tr>
               <td>
                 <Form.Control
+                  ref={fieldRef}
                   name="field"
                   type="text"
-                  onChange={formChangeHandler}
+                  onChange={addField}
                 />
               </td>
               <td>
                 <Form.Control
+                  ref={typeRef}
                   name="type"
                   type="text"
-                  onChange={formChangeHandler}
+                  onChange={addType}
                 />
               </td>
               <td>
-                <Button > + </Button>
+                <Button onClick={addRow}> + </Button>
               </td>
             </tr>
           </Table>
         </Form.Group>
         <Button onClick={addModel}>Save</Button>
       </Form>
-    </Container>
+    </div>
   );
 };
 

44c43ff0bbbc5f105eb12d3595b3d4cb8c2ed0c1
manny <33538272+Basic-Sombrero@users.noreply.github.com>
2021-06-15 22:03:41 -0700

    added documentation to user services

--- backend/UserService/src/main/java/com/example/userservice/repository/UserRepo.java
+++ backend/UserService/src/main/java/com/example/userservice/repository/UserRepo.java
@@ -2,13 +2,18 @@ package com.example.userservice.repository;
 
 import com.example.userservice.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.bson.types.ObjectId;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface UserRepo extends MongoRepository<User, String > {
 
     /*Queries*/
+
+    /**
+     *
+     * @param email The email of the of the user
+     * @return User after calling the query to find the user by their email
+     */
     User findByEmail(String email);
 }

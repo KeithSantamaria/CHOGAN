49e5a6bf1f23b0c37b54cec3782856d3b0a024c7
TJ <23747199+tomjmartinez@users.noreply.github.com>
2021-06-16 01:43:40 -0700

    tests passing

--- frontend/src/__tests__/Chogan.test.tsx
+++ frontend/src/__tests__/Chogan.test.tsx
@@ -3,13 +3,16 @@ import { shallow } from 'enzyme';
 import HomeListView from '../components/home/HomeListView';
 import Chogan from '../Chogan';
 
+import Enzyme, { configure, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
 describe('HomeListView Tests', () => {
 
     it('renders list-items', () => {
-      const items = ['one', 'two', 'three'];
       const wrapper = shallow(<Chogan/>);
   
       // Expect the wrapper object to be defined
-      expect(wrapper.find('.App')).toBeDefined();
+      expect(wrapper.find('.Apps')).toBeDefined();
     });
   });

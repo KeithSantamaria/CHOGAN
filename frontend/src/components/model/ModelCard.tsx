932199e59c10abac62969bad461273ea95f89a02
scbarnard01 <39101603+scbarnard@users.noreply.github.com>
2021-06-16 12:56:49 -0400

    v8

--- frontend/src/components/model/ModelCard.tsx
+++ frontend/src/components/model/ModelCard.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { useAppDispatch } from "../../redux/hooks";
 
 const ModelCard = ({ model }: any) => {
   const [metadata] = React.useState(model.modelMetadata);
+  const dispatch = useAppDispatch();
+
+  const removeEndpoint = (id: string) => {
+    const queryDeleteString = `http://localhost:42069/api/delete/project/endpoint`;
+    const body = {
+      params: {
+        endpointId: endpoint.endpointId,
+      },
+    };
+    console.log("request body: ", queryDeleteString, body);
+
+    axios
+      .delete(queryDeleteString, body)
+      .then((response) => {
+        console.log("response", response);
+        const tagData = response.data;
+        dispatch(setEndpoints(tagData));
+      })
+      .catch((error) => {
+        console.log("There was an error on deleting: ", error);
+      });
+  };
+
   return (
     <Card>
       <Card.Body>

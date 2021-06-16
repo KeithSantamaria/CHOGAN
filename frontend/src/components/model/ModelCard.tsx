2d49678ebb986c2f96f8567b1b82c4ecbd333ca6
scbarnard01 <39101603+scbarnard@users.noreply.github.com>
2021-06-16 13:29:06 -0400

    flacid models

--- frontend/src/components/model/ModelCard.tsx
+++ frontend/src/components/model/ModelCard.tsx
@@ -1,26 +1,31 @@
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { useAppDispatch } from "../../redux/hooks";
+import { setModels } from "../../redux/projectAppSlice";
 
 const ModelCard = ({ model }: any) => {
   const [metadata] = React.useState(model.modelMetadata);
+  const [modelId] = React.useState(model.modelId);
   const dispatch = useAppDispatch();
 
-  const removeEndpoint = (id: string) => {
-    const queryDeleteString = `http://localhost:42069/api/delete/project/endpoint`;
+  const removeModel = (id: string) => {
+    const queryDeleteString = `http://localhost:42069/api/delete/project/model`;
     const body = {
       params: {
-        endpointId: endpoint.endpointId,
+        modelId: modelId,
       },
     };
-    console.log("request body: ", queryDeleteString, body);
+    console.log(body);
 
     axios
       .delete(queryDeleteString, body)
       .then((response) => {
         console.log("response", response);
-        const tagData = response.data;
-        dispatch(setEndpoints(tagData));
+        const modelData = response.data;
+        dispatch(setModels(modelData));
       })
       .catch((error) => {
         console.log("There was an error on deleting: ", error);
@@ -34,8 +39,8 @@ const ModelCard = ({ model }: any) => {
         {/* {Object.values(metadata).forEach( (value:any) => {
           <Card.Text>{value}</Card.Text>
         })} */}
-        {Object.keys(metadata).forEach( (key:any) => {
-          <Card.Text>{metadata[key]}</Card.Text>
+        {Object.keys(metadata).forEach((key: any) => {
+          <Card.Text>{metadata[key]}</Card.Text>;
         })}
         {/* {Object.keys(metadata).forEach( key => {
           return <Card.Text>{metadata[key]}</Card.Text>
@@ -43,6 +48,17 @@ const ModelCard = ({ model }: any) => {
         {/* {Object.entries(metadata).forEach((entry:any) => {
           return <Card.Text>{entry}</Card.Text>;
         })} */}
+        <Button
+          value={model.modelId}
+          onClick={(e) => removeModel((e.target as HTMLButtonElement).value)}
+          variant="outline-dark"
+        >
+          <FontAwesomeIcon
+            style={{ height: "100%" }}
+            className="fa-1x"
+            icon={faTrash}
+          />
+        </Button>
       </Card.Body>
     </Card>
   );

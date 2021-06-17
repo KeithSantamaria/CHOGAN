7ae03d77f6aff5bd538b3d19b7a1b3fc98e25de6
Kevin <kdalt412@gmail.com>
2021-06-16 21:32:54 -0700

    Fixed ListView dispatch
    yup

--- frontend/src/components/home/ProjectList.component.tsx
+++ frontend/src/components/home/ProjectList.component.tsx
@@ -4,17 +4,21 @@ import ProjectElipsisBtn from './modal/ProjectElipsisBtn';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faClipboardList} from "@fortawesome/free-solid-svg-icons";
 import { useHistory } from 'react-router-dom';
+import { useAppDispatch } from '../../redux/hooks';
+import { setProject } from '../../redux/projectAppSlice';
 
 
 export default function ProjectList(props:any) {
   const [renderCard, setRenderCard] = useState([]);
   const history = useHistory();
+  const dispatch = useAppDispatch();
   
   useEffect(() => {
     setRenderCard(props.projects)
   }, [props.projects]);
 
-  const goToProject = () => {
+  const goToProject = (project: any) => {
+    dispatch(setProject(project));
     history.push("/user/project/general");    
   }; 
 
@@ -23,7 +27,7 @@ export default function ProjectList(props:any) {
       {renderCard.map((project:any) => (
          <ListGroup.Item className="project-list-item">
             <Row>
-              <Col className="project-name" onClick={goToProject}>
+              <Col className="project-name" onClick={() => goToProject(project)}>
                   <FontAwesomeIcon style={{height: '40%', marginRight: '10px'}}className="fa-1x" icon={faClipboardList}/> 
                   {project.projectName}
               </Col>
